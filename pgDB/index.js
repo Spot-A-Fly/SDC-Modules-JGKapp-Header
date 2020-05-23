357ab753357a62c364dbc6a63d75054c8dc19a57
Joel Kapp <joel.g.kapp@gmail.com>
2020-05-23 15:00:53 -0600

    removed support for mongoDB and reorganized folders

--- pgDB/index.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  database: 'artists',
-});
-
-module.exports = {
-  query: (text, params, callback) => pool.query(text, params, callback),
-};
