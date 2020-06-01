7353e482358e3bb0c460816c07b08fe016d58258
Joel Kapp <joel.g.kapp@gmail.com>
2020-06-01 15:08:05 -0600

    Implemnt and update artillery tests

--- stress-test-functions.js
+++ stress-test-functions.js
@@ -4,4 +4,13 @@ module.exports = {
     context.vars.id = Math.floor(Math.random() * 10000000);
     return done();
   },
+  getRandomIdInLastHalf: (context, events, done) => {
+    context.vars.id = Math.floor(Math.random() * 5000000) + 5000000;
+    return done();
+  },
+  getFrequentlyUsedId: (context, events, done) => {
+    // Return one of 50 frequented Id's
+    context.vars.id = Math.floor(10000000 / Math.floor((Math.random() * 50 + 1)));
+    return done();
+  },
 };
