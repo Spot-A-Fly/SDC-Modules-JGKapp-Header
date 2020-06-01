e28d5ee536ef7b20e6c5d6bbc54ae50c95e7a674
Joel Kapp <joel.g.kapp@gmail.com>
2020-06-01 16:21:56 -0600

    Update environment to be able to use remote database

--- config.js
+++ config.js
@@ -32,11 +32,11 @@ const prod = {
     port: parseInt(process.env.PROD_APP_PORT, 10) || 3000,
   },
   db: {
-    host: process.env.TEST_DB_HOST || 'database',
+    host: process.env.PROD_DB_HOST || 'database',
     port: parseInt(process.env.TEST_DB_PORT, 10) || 5432,
     database: process.env.TEST_DB_NAME || 'artists',
     user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
+    password: process.env.PG_AWS_PASSWORD,
   },
 };
 
