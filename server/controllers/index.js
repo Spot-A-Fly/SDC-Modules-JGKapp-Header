0af6ae2b6432b6e7723c1a64d58681272f332c53
Joel Kapp <joel.g.kapp@gmail.com>
2020-06-01 18:48:28 -0600

    Implement cache in controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,4 @@
+const cache = require('../cache/index.js');
 const model = require('../models/index.js');
 
 module.exports = {
@@ -11,23 +12,50 @@ module.exports = {
     } else {
       let modelFunction = '';
       let param = '';
+      let cacheKey = '';
       if (artistName !== undefined) {
         modelFunction = model.getArtistByName;
         param = artistName;
+        cacheKey = `artistName=${artistName}`;
       } else if (artistId !== undefined) {
         modelFunction = model.getArtistById;
         param = artistId;
+        cacheKey = `artistId=${artistId}`;
       }
 
-      modelFunction(param)
-        .then((artist) => res.status(200).json({
-          message: 'Successfully retrieved artist',
-          artist: artist.rows,
-        }))
-        .catch((err) => res.status(400).json({
-          message: 'Failed to find artist',
-          error: err,
-        }));
+      // With Cache
+      cache.retrieveFromCache(cacheKey)
+        .then((results) => {
+          if (results) {
+            res.status(200).json(results);
+          } else {
+            modelFunction(param)
+              .then((artist) => {
+                const messageObj = {
+                  message: 'Successfully retrieved artist',
+                  artist: artist.rows,
+                };
+                res.status(200).json(messageObj);
+                return messageObj;
+              })
+              .then((value) => cache.addToCache(cacheKey, value))
+              .catch((err) => res.status(400).json({
+                message: 'Failed to find artist',
+                error: err,
+              }));
+          }
+        });
+
+      // Without Cache
+      // modelFunction(param)
+      //   .then((artist) => res.status(200).json({
+      //     message: 'Successfully retrieved artist',
+      //     artist: artist.rows,
+      //   }))
+      //   .catch((err) => res.status(400).json({
+      //     message: 'Failed to find artist',
+      //     error: err,
+      //   }));
     }
   },
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-console */
+// eslint-disable-next-line import/order
+const config = require('../config.js');
+require('newrelic');
+
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 const artistRouter = require('./router/index.js');
-const config = require('../config.js');
 
 const app = express();
 
@@ -14,24 +17,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/../public')));
-
-// // Controller for GET by id
-// app.get('/data/artist/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   db.getArtist(id)
-//     .then(artist => res.json(artist))
-//     .catch(console.log);
-// });
-
-// // Controller for POST
-// app.post('/data/artist', (req, res) => {
-//   const { name } = req.body;
-//   const { header_img } = req.body;
-
-//   db.save({ name, header_img });
-//   res.status(201).json();
-// });
 
 // router
 app.use('/data/artist', artistRouter);
